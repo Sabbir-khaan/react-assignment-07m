acb245cfbe6c02cf59a30596ae73d1514f93e6db
Sabbir-khaan <shabbirkhan55660@gmail.com>
2025-04-15 23:39:22 +0600

    added navbar and other necessary files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-regular-svg-icons'
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center max-w-[90%] mx-auto mt-6'>
+            <div>
+                <h1><span className='text-[#003EA4] text-3xl'>Auction</span><span className='text-[#FFD337] font-bold text-3xl'>Gallery</span></h1>
+            </div>
+            <div>
+                <ul className='flex gap-[4.8rem] text-[1.3rem]'>
+                    <li>Home</li>
+                    <li>Auctions</li>
+                    <li>Categories</li>
+                    <li>How To Works</li>
+                </ul>
+            </div>
+            <div className='flex gap-5'>
+                <div className='bg-[#EBF0F5] text-center p-1 rounded-full w-[2rem]'>
+                    <FontAwesomeIcon icon={faBell}></FontAwesomeIcon>
+                </div>
+                <div>
+                    <img className='w-[2rem]' src="/src/assets/login.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
